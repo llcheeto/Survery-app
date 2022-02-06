5abf462642447bbcf0ce5e9f98a57d4d03b11fb2
llcheeto <llcheeto@github.com>
2022-02-06 14:55:32 -0500

    adding logout feature

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
+import { connect  } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default:
+        return <li><a>Logout</a></li>;
+    }
+  }
   render() {
     return (
       <nav>
@@ -9,14 +22,16 @@ class Header extends Component {
             Emaily
           </a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     )
   }
 };
+function mapStateToProps({ auth }) {
+  return { auth };
+}
 
-export default Header
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,13 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from "../actions/types";
+
+const authReducer  = (state = null, action) => {
   console.log (action);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
 }
+
+export default authReducer;
