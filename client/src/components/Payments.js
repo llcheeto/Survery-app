484970594ef4ed8564a7ac8c4b679767b66defe3
llcheeto <llcheeto@github.com>
2022-02-08 18:39:43 -0500

    set up stripe keys and api

---
+++ client/src/components/Payments.js
@@ -0,0 +1,18 @@
+import react, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout 
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      />
+    );
+  }
+}
+
+
+export default Payments;
