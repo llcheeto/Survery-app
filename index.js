c63f02f2089ed2f14b28616488de0a01d0997fa8
llcheeto <llcheeto@github.com>
2022-02-16 04:03:19 -0500

    SurveySchema and route handlers

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser =require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./servicecs/passport');
 
 
@@ -23,8 +24,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 require('./routes/authRoutes') (app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveryRoutes')(app);
 
 if (process.env.NODE_ENV === 'production' ) {
   // Express will serve up production assets
