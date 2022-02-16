28922cb38f8add51b8847962e68f21085b642825
llcheeto <llcheeto@github.com>
2022-02-16 05:54:31 -0500

    fixed survey typo

--- servicecs/Mailer.js
+++ servicecs/Mailer.js
@@ -6,6 +6,8 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('Briancornine@gmail.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -30,10 +32,29 @@ class Mailer extends helper.Mail {
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
   }
+  
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+  
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v/mail/send',
+      body: this.toJSON()
+    });
+  
+    const response = this.sgApi.API(request);
+    return response;
+  }
 }
 
 
 module.exports = Mailer;
 
 
-//
